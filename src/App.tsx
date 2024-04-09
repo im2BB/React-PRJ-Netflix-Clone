f1c0439b285fe1214b9a19ffb17106e8d0d57d8d
im222b <rudgnsdleh@naver.com>
2024-04-09 16:47:56 +0900

    upupup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import {BrowserRouter as Router,Route, Routes } from "react-router-dom";
-import Home from "./Routes";
+import Home from "./Routes/Home";
 import Tv from "./Routes/Tv";
 import Search from "./Routes/Search";
 import Header from "./Routes/Components/Header";
